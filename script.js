09954c219f3a86f68c1fa4c1070c0887250597fd
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2021-12-29 00:10:17 +0800

    Build the buttons' functionalities

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
+// Important Variables
 const container = document.querySelector('.container');
-const body = document.querySelector('body');
+const colorModeBtn = document.querySelector('#color-mode');
+const rainbowModeBtn = document.querySelector('#rainbow-mode');
+const eraserBtn = document.querySelector('#eraser');
+const clearBtn = document.querySelector('#clear');
 
-const size = 64;
+//Create a function that makes a grid on the given number
+function makeRows(rows, cols){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++){
+        let cell = document.createElement('div');
+        container.appendChild(cell).className = 'grid-item';
+    };
+};
 
+makeRows(16, 16);
 
+const cells = document.querySelectorAll('.grid-item');
+
+// Create a function that generates random number between 0 and 255
+function randomBetween(min, max){
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+// Create a function that 'draws' in the grid
+function colorMode(color){
+    cells.forEach(cell => {
+        cell.addEventListener('mouseenter', (e) => {
+            e.target.style.background = color;
+        });
+    });
+}
+
+colorModeBtn.addEventListener('click', () => {
+    colorMode('black');
+});
+
+// Create a function that draws in the grid with random colors
+rainbowModeBtn.addEventListener('click', () =>{
+    cells.forEach(cell => {
+        cell.addEventListener('mouseenter', () =>{
+            cell.setAttribute('style', `background-color: 
+            rgb(${randomBetween(0, 255)}, 
+            ${randomBetween(0, 255)}, 
+            ${randomBetween(0, 255)});`);
+        });
+        
+    })
+});
+
+// Create a function that 'erases' the drawing in the grid
+eraserBtn.addEventListener('click', () => {
+    colorMode('white')
+});
+
+// Create a function that 'clears' the grid
+clearBtn.addEventListener('click', () =>{
+    cells.forEach(cell => {
+        cell.style.background = 'white';
+    });
+})
 
 
 
